69892980b4a0acb32b9352b17d6512e5df0e54c0
Edison Lee <edisonlee@lawn-128-61-24-168.lawn.gatech.edu>
2023-09-26 19:39:44 -0400

    deleteuser

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -18,8 +18,29 @@ const updateUser = asyncHandler(async (req, res) => {
 });
 
 const deleteUser = asyncHandler(async (req, res) => {
-  // TODO: Replace with actual code
-  res.send("This is a test endpoint (deleteUser)");
+  const {id} = req.body
+
+  if (!id) {
+    return res.status(400).json({message: 'User ID Required'})
+  }
+
+  const notes = await Note.findOne ({ user: id}).lean().exec()
+
+  if(notes?.length){
+    return res.status(400).json({ message: 'User has assigned notes'})
+  }
+  const user = await User.findById(id).exec()
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not found'})
+  }
+
+  const result = await user.deleteOne()
+
+  const reply = 'Username ${results.username} with ID ${result._id}
+  deleted'
+
+  res.json(reply)
 });
 
 module.exports = {
