c39937d875da6f3906826a2124d8c9d0f356bcbc
Hebe3834 <hebeluna00@gmail.com>
2023-09-26 16:14:20 -0400

    gitignore update

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: [{
+        type: String,
+        default: "Non-Member"
+    }]
+})
+
+module.exports = mongoose.model('User', userSchema)
