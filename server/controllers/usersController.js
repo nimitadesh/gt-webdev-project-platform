2593d3ae9906ffbd6d1a453f991b2a1175e36d55
Alex <alex@lawn-143-215-61-145.lawn.gatech.edu>
2023-09-29 14:33:36 -0400

    getAllUsers

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -4,6 +4,11 @@ const bcrypt = require("bcrypt");
 
 const getAllUsers = asyncHandler(async (req, res) => {
   // TODO: Replace with actual code
+  const users = await User.find().select('-password').lean()
+  if (!users) {
+    return res.status(400).json({ message: 'No users found'})
+  }
+  res.json(users)
   res.send("This is a test endpoint (getAllUsers)");
 });
 
