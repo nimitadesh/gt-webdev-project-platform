149e1da10ae666c44edee001850d2f68f7f52b3a
Nimita Deshpande <nimita.ahs@gmail.com>
2023-10-02 11:22:46 -0400

    Fixed MongoDB connection issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import logo from "./logo.svg";
+// import "./App.css";
 
 function App() {
   const [firstName, setFirstName] = useState("");
--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: ".env" });
 const express = require("express");
 const app = express();
 const port = 3001;
 const cors = require("cors");
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
+
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB!");
+});
 
 app.use(cors());
 app.use(express.json());
