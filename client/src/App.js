0a5eff64a5c19694ad61d43466ca4337e9670611
Hebe3834 <hebeluna00@gmail.com>
2023-10-06 16:36:08 -0400

    created home.jsx react component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
 import React, { useState } from "react";
 
 function App() {
@@ -8,7 +9,7 @@ function App() {
       <div className="App">
         {/* <Login /> */}
         <Routes>
-          {/* { <Route path="/" element={<Home />} /> } */}
+          { <Route path="/" element={<Home />} /> }
           {<Route path="/login" element={<Login />} />}
           {/* {<Route path="/signup" element={<Signup />} /> } */}
         </Routes>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,1 +1,2 @@
 export {default as Login} from './Login.jsx'
+export {default as Home} from './Home.jsx'
