9e36cecf59d4a2a12bd0e9a2c6820f38f9a1daa5
Hebe3834 <hebeluna00@gmail.com>
2023-10-17 16:47:02 -0400

    added project card component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import * as React from 'react';
+import * as React from "react";
+import ProjectCard from "./ProjectCard";
 
 const Home = () => {
-    return <h1>Home</h1>;
-}
+  return (
+    <div>
+      <h1>Home</h1>
+      <ProjectCard
+        project={{
+          _id: "01",
+          projectTitle: "title1",
+          description: "test desc",
+        }}
+      />
+    </div>
+  );
+};
 
 export default Home;
