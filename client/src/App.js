91f851b6bdc9b63fcef1073e0136f23c0268ac77
Nimita Deshpande <nimita.ahs@gmail.com>
2023-10-20 15:12:34 -0400

    Testing ProjectCard locally

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
+import ProjectCard from "./pages/ProjectCard.jsx";
 import React, { useState } from "react";
+
+const testProject = {
+  _id: 1,
+  projectTitle: "Project Title",
+  description: "Project Description",
+};
 
 function App() {
   return (
@@ -9,6 +16,7 @@ function App() {
       <div className="App">
         {/* <Login /> */}
         <Routes>
+          {/* {<Route path="/" element={<ProjectCard project={testProject} />} />} */}
           {<Route path="/" element={<Home />} />}
           {<Route path="/login" element={<Login />} />}
           {/* {<Route path="/signup" element={<Signup />} /> } */}
