8895fbd5d0d66f676587920141711eb8d150178a
Hebe3834 <hebeluna00@gmail.com>
2023-10-20 16:45:13 -0400

    created basic responsive navbar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import ProjectCard from "./ProjectCard";
+import IndividualProject from "./IndividualProject";
+import NavBar from "./NavBar";
 
 const Home = () => {
   return (
     <div>
+      <NavBar />
       <h1>Home</h1>
       <ProjectCard
         project={{
