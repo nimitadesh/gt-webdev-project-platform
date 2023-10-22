d0a1085dbc36545165bdd03593ac8a80708f69ca
Nimita Deshpande <nimita.ahs@gmail.com>
2023-10-22 16:47:08 -0400

    Completed login and signup frontend and backend connection

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,41 @@
-import * as React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 import ProjectCard from "./ProjectCard";
 
 const Home = () => {
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+  const [username, setUsername] = useState("");
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+      }
+      const { data } = await axios.post(
+        "http://localhost:3001",
+        {},
+        { withCredentials: true }
+      );
+      const { status, user } = data;
+      setUsername(user);
+      return status
+        ? toast(`Hello ${user}`, {
+            position: "top-right",
+          })
+        : (removeCookie("token"), navigate("/login"));
+    };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+  const Logout = () => {
+    removeCookie("token");
+    navigate("/login");
+  };
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Welcome, <span>{username}</span></h1>
       <ProjectCard
         project={{
           _id: "01",
@@ -12,6 +43,7 @@ const Home = () => {
           description: "test desc",
         }}
       />
+      <button onClick={Logout}>LOGOUT</button>
     </div>
   );
 };
