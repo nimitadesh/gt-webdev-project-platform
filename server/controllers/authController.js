4188dbbf4b391dae9fdee3ee18aa0d9d08b87860
Nimita Deshpande <nimita.ahs@gmail.com>
2023-10-22 15:03:36 -0400

    Added login backend logic

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -32,3 +32,31 @@ module.exports.Signup = async (req, res, next) => {
     console.error(error);
   }
 };
+
+module.exports.Login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.json({ message: "Incorrect password or username" });
+    }
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res.json({ message: "Incorrect password or username" });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
