53305451367320a35252e019d4fc16cd04227557
Nimita Deshpande <nimita.ahs@gmail.com>
2023-10-22 15:03:36 -0400

    Added login backend logic

--- server/routes/AuthRoute.js
+++ server/routes/AuthRoute.js
@@ -2,5 +2,6 @@ const { Signup } = require("../controllers/authController");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
 
 module.exports = router;
