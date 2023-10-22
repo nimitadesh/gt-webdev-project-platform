2df29909a0231cec1856b93850dfbd40e3460a89
Nimita Deshpande <nimita.ahs@gmail.com>
2023-10-22 15:57:02 -0400

    Connected login frontend and backend

--- server/routes/projectRoutes.js
+++ server/routes/projectRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const projectsController = require("../controllers/projectController");
+const projectsController = require("../controllers/ProjectController");
 
 router
   .get("/", projectsController.getAllProjects)
