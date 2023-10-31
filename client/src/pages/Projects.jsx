04b11fec9c0fcf027c389027da0160f010f7db7b
Edison Lee <edisonlee@lawn-128-61-29-238.lawn.gatech.edu>
2023-10-31 18:45:43 -0400

    completed navbar pages

---
+++ client/src/pages/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./NavBar"; // Import the NavBar component
+
+const Projects = () => {
+  return (
+    <div>
+      <NavBar /> {/* Include the navigation bar */}
+      <h1>Projects</h1>
+      {/* Other content for the Projects page */}
+    </div>
+  );
+};
+
+export default Projects;
