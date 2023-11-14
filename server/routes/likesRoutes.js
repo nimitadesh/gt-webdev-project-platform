35afff91d03a483e13185469dfddf9414b4d7945
Hebe3834 <hebeluna00@gmail.com>
2023-11-14 17:35:34 -0500

    small change

--- server/routes/likesRoutes.js
+++ server/routes/likesRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const projectsController = require("../controllers/likesController");
 
-router.get("/", likesController.createNewLike);
+router.get("/like", likesController.createNewLike);
 
 module.exports = router;
