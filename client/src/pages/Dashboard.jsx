03a1d78b6a7093792e78b68931f7a7081775b7c6
Hebe3834 <hebeluna00@gmail.com>
2023-11-14 16:38:05 -0500

    tried to display info on dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,31 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import NavBar from "./NavBar";
 
 const Dashboard = () => {
+  const { username } = useParams();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/dashboard/${username}`)
+      .then((response) => response.json())
+      .then((data) => setUser(data))
+      .catch((error) => console.log(error));
+  }, [username]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
       <NavBar />
-      ...
+      <div className="Dashboard">
+        <h1>
+          {user.firstName} {user.lastName}
+        </h1>
+        <a href={user.github}>Github</a>
+      </div>
     </div>
   );
 };
