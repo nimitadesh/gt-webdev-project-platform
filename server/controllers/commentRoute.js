68571453dba50dce0f1d0875a6d40d5b190bfd54
Nimita Deshpande <nimita.ahs@gmail.com>
2023-11-25 18:49:57 -0500

    Backend commenting added

--- server/controllers/commentRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const CommentController = require('../controllers/commentController');
-
-// Define routes for adding comments
-router.post('/comments', CommentController.addComment);
-
-// Export the router
-module.exports = router;
